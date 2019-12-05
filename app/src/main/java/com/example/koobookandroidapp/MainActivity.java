5ff31a67b7af26abd453a0391971fb33cda71308
Kane <kaned@DESKTOP-AMNRVQO>
2019-12-05 13:59:11 +0000

    Added method for building room database

--- app/src/main/java/com/example/koobookandroidapp/MainActivity.java
+++ app/src/main/java/com/example/koobookandroidapp/MainActivity.java
@@ -1,15 +1,21 @@
 package com.example.koobookandroidapp;
 
+import android.arch.persistence.room.Room;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    AppDatabase db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        BuildRoomDatabase();
+    }
 
-        //Test 2345
+
+    public void BuildRoomDatabase(){
+        db = Room.databaseBuilder(getApplicationContext(), AppDatabase.class, "production").allowMainThreadQueries().build();
     }
 }
