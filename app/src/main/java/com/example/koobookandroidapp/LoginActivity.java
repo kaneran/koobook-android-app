1900c52f640ff18ab866343cff99867f0dfdb2a9
Kane <kaned@DESKTOP-AMNRVQO>
2019-12-05 15:41:54 +0000

    Removed method for building room database

--- app/src/main/java/com/example/koobookandroidapp/LoginActivity.java
+++ app/src/main/java/com/example/koobookandroidapp/LoginActivity.java
@@ -11,12 +11,12 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.start_fragment);
-        db = Room.databaseBuilder(getApplicationContext(), AppDatabase.class, "production").allowMainThreadQueries().build();
+
     }
 
 
     public void BuildRoomDatabase(){
-
+        db = Room.databaseBuilder(getApplicationContext(), AppDatabase.class, "production").allowMainThreadQueries().build();
 
     }
 }
