f1c546408c61ba52ccb546938ee27e28623ef724
Kane <kaned@DESKTOP-AMNRVQO>
2019-12-11 15:22:27 +0000

    Added logic to check all the fields are filled in the sign up page and ensure that the correct error messages were displayed on screen. Also made the method for randomly generating a key

--- app/src/main/java/Boundaries/SignUpActivity.java
+++ app/src/main/java/Boundaries/SignUpActivity.java
@@ -1,6 +1,5 @@
 package Boundaries;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -9,24 +8,62 @@ import android.widget.TextView;
 
 import com.example.koobookandroidapp.R;
 
+import java.util.ArrayList;
+import java.util.Dictionary;
+import java.util.HashMap;
+
 import User.UserController;
 
 public class SignUpActivity extends AppCompatActivity {
-    TextView firstNamerequiredTextView;
+    TextView firstNameRequiredTextView;
+    TextView emailRequiredTextView;
+    TextView passwordRequiredTextView;
+    TextView passwordDoesNotMatchTextView;
     EditText firstNameEditText;
-
+    EditText emailEditText;
+    EditText passwordEditText;
+    EditText confirmPasswordEditText;
+    ArrayList<EditText> passwords;
+    HashMap<EditText,TextView> map;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
-        firstNamerequiredTextView = findViewById(R.id.firstNameRequired);
+
+        map = new HashMap<>();
+        firstNameRequiredTextView = findViewById(R.id.firstNameRequiredTextView);
         firstNameEditText = findViewById(R.id.firstNameEditText);
+        map.put(firstNameEditText, firstNameRequiredTextView);
+
+
+        emailRequiredTextView = findViewById(R.id.emailRequiredTextView);
+        emailEditText = findViewById(R.id.emailEditText);
+        map.put(emailEditText, emailRequiredTextView);
+
+        passwordRequiredTextView = findViewById(R.id.passwordsRequiredTextView);
+        passwordEditText = findViewById(R.id.passwordEditText);
+        map.put(passwordEditText, passwordRequiredTextView);
+
+
+        confirmPasswordEditText = findViewById(R.id.confirmPasswordEditText);
+        passwordDoesNotMatchTextView = findViewById(R.id.passwordsDoesNotMatchTextView);
+
+        //I added the "Password does not match text view to the hashmap because I plan to access it
+        // and modify the string dpeending on whether the user entered mismatching passwords
+        // or didn't enter anything at all."
+        map.put(confirmPasswordEditText, passwordDoesNotMatchTextView);
+
+
+        passwords = new ArrayList<>();
+        passwords.add(passwordEditText);
+        passwords.add(confirmPasswordEditText);
     }
 
     public void signUpButtonClicked(View v){
 
         UserController userController = new UserController();
-        userController.checkIfUserEnteredInformationInAllFields(firstNamerequiredTextView,firstNameEditText);
+        userController.checkIfUserEnteredInformationInAllFields(map, passwordDoesNotMatchTextView);
+        userController.checkIfPasswordsMatch(passwords, passwordDoesNotMatchTextView);
     }
 }
--- app/src/main/java/User/UserController.java
+++ app/src/main/java/User/UserController.java
@@ -1,19 +1,74 @@
 package User;
 
+import android.content.res.ColorStateList;
+import android.graphics.Color;
+import android.support.v4.view.ViewCompat;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
 import com.example.koobookandroidapp.R;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import Colour.ColorManager;
+
 public class UserController {
     TextView firstNamerequiredTextView;
     EditText firstNameEditText;
+    EditText passwordEditText;
+    EditText confirmedPasswordEditText;
 
 
-    public void checkIfUserEnteredInformationInAllFields(TextView firstNamerequiredTextView, EditText firstNameEditText ){
-        if(firstNameEditText.getText().toString().matches("")){
-            firstNamerequiredTextView.setVisibility(View.VISIBLE);
+    public void checkIfUserEnteredInformationInAllFields(HashMap<EditText,TextView> map, TextView passwordDoesNotMatchTextView){
+        ColorManager colorManager = new ColorManager();
+        for( EditText editText: map.keySet()){
+            TextView fieldRequiredTextView = map.get(editText);
+            if(editText.getText().toString().matches("")){
+
+
+                //If the text view is for "confirmed password" then I must override its string value
+                fieldRequiredTextView.setText(R.string.this_field_is_required);
+                fieldRequiredTextView.setVisibility(View.VISIBLE);
+                colorManager.setBackgroundTint(editText,"#D81B60");
+
+            } else{
+                colorManager.setBackgroundTint(editText,"#FFFFFF");
+                fieldRequiredTextView.setVisibility(View.INVISIBLE);
+
+                //Did this to ensure that the overwritten message for the text view was correct
+                if(fieldRequiredTextView.equals(passwordDoesNotMatchTextView)){
+                    fieldRequiredTextView.setText(R.string.passwords_does_not_match);
+
+                }
+
+            }
+        }
+
+    }
+
+    public boolean checkIfPasswordsMatch(ArrayList<EditText> passwords, TextView passwordDoesNotMatchTextView){
+        ColorManager colorManager = new ColorManager();
+
+        if(passwordDoesNotMatchTextView.getText().toString().matches("This field is required")){
+            return false;
+        } else{
+
+            passwordEditText = passwords.get(0);
+            confirmedPasswordEditText = passwords.get(1);
+            if(!passwordEditText.getText().toString().matches(confirmedPasswordEditText.getText().toString())){
+                passwordDoesNotMatchTextView.setText(R.string.passwords_does_not_match);
+                passwordDoesNotMatchTextView.setVisibility(View.VISIBLE);
+                colorManager.setBackgroundTint(confirmedPasswordEditText,"#D81B60");
+                colorManager.setBackgroundTint(passwordEditText,"#D81B60");
+                return false;
+            } else{
+                BlowfishController blowfishController = new BlowfishController();
+                String key = blowfishController.generateKey();
+
+                return true;
+            }
 
         }
 
