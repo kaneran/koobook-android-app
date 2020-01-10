05b1ac410dfb78407d151c71dc98a40ce5eefc17
Kane <kaned@DESKTOP-AMNRVQO>
2020-01-10 15:33:12 +0000

    Was able to retrieve the book data, split it and create a hashmap and split any data, in the hashmap, that contains more than one value as indicated by the "#" and "$" symbol

--- app/src/main/java/controllers/BookController.java
+++ app/src/main/java/controllers/BookController.java
@@ -10,6 +10,9 @@ import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.InputStreamReader;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 import dataaccess.setup.AppDatabase;
 import entities.Book;
@@ -18,6 +21,10 @@ public class BookController extends AsyncTask<String, Void, Boolean> {
     AppDatabase db;
     Book book;
     String isbn;
+    HashMap<BookData,String> bookDataMap;
+    String[] genres;
+    String[] reviews;
+    String[] authors;
 
     //Checks in room database to see if book exists
     public boolean checkIfBookExistsInDatabase(Context context) {
@@ -64,7 +71,7 @@ public class BookController extends AsyncTask<String, Void, Boolean> {
     public void receiveBookInformation(String... strings){
         byte[] messageByte = new byte[1000];
         boolean end = false;
-        String dataString = "";
+        String bookDataString = "";
         String fromReceiver = "";
         try{
             String data;
@@ -74,13 +81,13 @@ public class BookController extends AsyncTask<String, Void, Boolean> {
             DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
 
             //Send isbn number to the server
-            outToServer.writeBytes(isbn);
+            outToServer.writeBytes(isbn + "#");
 
             //Receive the book data from the server
             while(!end){
                 int bytesRead = in.read(messageByte);
-                dataString += new String(messageByte, 0, bytesRead);
-                if(dataString.length()> 0){
+                bookDataString += new String(messageByte, 0, bytesRead);
+                if(bookDataString.length()> 0){
                     end = true;
                 }
 
@@ -105,11 +112,96 @@ public class BookController extends AsyncTask<String, Void, Boolean> {
                 }
             }
             clientSocket.close();
+            bookDataMap = EncodeBookData(bookDataString);
+            genres = SplitStringsAndPutIntoList(bookDataMap.get(BookData.Genres));
+            authors = SplitStringsAndPutIntoList(bookDataMap.get(BookData.Authors));
+            reviews = SplitStringsAndPutIntoList(bookDataMap.get(BookData.AmazonReviews));
         } catch (Exception e){
             e.printStackTrace();
         }
 
+    }
+    public HashMap<BookData,String> EncodeBookData(String bookData) {
+        char[] chars  = bookData.toCharArray();
+        HashMap<BookData,String> bookDataMap = new HashMap<BookData,String>();
+        StringBuilder sb = new StringBuilder();
+        int bookDataIndex = 0;
+        for(int i =0; i<chars.length; i++) {
 
+            if(chars[i] == '$') {
+                bookDataIndex++;
+                switch(bookDataIndex){
+                    case 1:
+                        bookDataMap.put(BookData.Title, sb.toString());
+                    case 2:
+                        bookDataMap.put(BookData.Authors, sb.toString());
+                    case 3:
+                        bookDataMap.put(BookData.Description, sb.toString());
+                    case 4:
+                        bookDataMap.put(BookData.Subtitle, sb.toString());
+                    case 5:
+                        bookDataMap.put(BookData.Isbn, sb.toString());
+                    case 6:
+                        bookDataMap.put(BookData.GoogleBooksAverageRating, sb.toString());
+                    case 7:
+                        bookDataMap.put(BookData.GoodreadsAverageRating, sb.toString());
+                    case 8:
+                        bookDataMap.put(BookData.AmazonAverageRating, sb.toString());
+                    case 9:
+                        bookDataMap.put(BookData.AmazonFiveStarRatingPercentage, sb.toString());
+                    case 10:
+                        bookDataMap.put(BookData.AmazonFourStarRatingPercentage, sb.toString());
+                    case 11:
+                        bookDataMap.put(BookData.AmazonThreeStarRatingPercentage, sb.toString());
+                    case 12:
+                        bookDataMap.put(BookData.AmazonTwoStarRatingPercentage, sb.toString());
+                    case 13:
+                        bookDataMap.put(BookData.AmazonOneStarRatingPercentage, sb.toString());
+                    case 14:
+                        bookDataMap.put(BookData.AmazonReviews, sb.toString());
+                    case 15:
+                        bookDataMap.put(BookData.AmazonReviewsCount, sb.toString());
+                    case 16:
+                        bookDataMap.put(BookData.Genres, sb.toString());
+                    case 17:
+                        bookDataMap.put(BookData.PageCount, sb.toString());
+                }
+                sb.setLength(0);
 
+            }
+            else {
+                sb.append(chars[i]);
+
+            }
+
+        }
+
+        return bookDataMap;
+    }
+
+    public static String[] SplitStringsAndPutIntoList(String str){
+        String[] stringArray = str.split("#",5);
+
+        return stringArray;
+    }
+
+    public enum BookData {
+        Title,
+        Authors,
+        Description,
+        Subtitle,
+        Isbn,
+        GoogleBooksAverageRating,
+        GoodreadsAverageRating,
+        AmazonAverageRating,
+        AmazonFiveStarRatingPercentage,
+        AmazonFourStarRatingPercentage,
+        AmazonThreeStarRatingPercentage,
+        AmazonTwoStarRatingPercentage,
+        AmazonOneStarRatingPercentage,
+        AmazonReviews,
+        AmazonReviewsCount,
+        Genres,
+        PageCount
     }
 }
