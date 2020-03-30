f92a916f8f3ac7f6f0d740a51ac8e9cfbc19d66c
KaneD97 <kanedharan@gmail.com>
2020-03-30 14:02:04 +0100

    Modified the "My preferences" page to incorproate a modular interface to help increase the speed in which users are able to extract insights from

--- app/src/main/java/fragments/SummaryFragment.java
+++ app/src/main/java/fragments/SummaryFragment.java
@@ -1,17 +1,14 @@
 package fragments;
 
 
-import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.support.v7.widget.Toolbar;
 import android.util.Pair;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.TextView;
 
 
@@ -21,22 +18,27 @@ import java.util.ArrayList;
 import java.util.List;
 
 import controllers.AuthorController;
+import controllers.BookController;
 import controllers.GenreController;
 import controllers.ReviewsController;
-import entities.Book;
+import controllers.UserController;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class SummaryFragment extends Fragment {
-    TextView textview_most_liked_genre, textview_most_disliked_genre, textview_most_liked_author, textview_most_disliked_author, textview_reviews;
+    TextView textview_most_liked_genre, textview_most_disliked_genre, textview_most_liked_author, textview_most_disliked_author, textview_reviews, textview_books_liked_count, textview_books_disliked_count;
     AuthorController authorController;
     GenreController genreController;
     ReviewsController reviewsController;
+    BookController bookController;
+    UserController userController;
     TextView textview_toolbar_title;
     List<String> reviews;
-    Button button_get_definition_of_book;
+    int books_liked_count;
+    int books_disliked_count;
+    int userId;
     public SummaryFragment() {
         // Required empty public constructor
     }
@@ -53,16 +55,19 @@ public class SummaryFragment extends Fragment {
     public void onViewCreated(@NonNull final View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         textview_toolbar_title = getActivity().findViewById(R.id.toolbar_title);
-        textview_most_liked_genre = view.findViewById(R.id.textview_summary_most_liked_genre);
-        textview_most_disliked_genre = view.findViewById(R.id.textview_summary_most_disliked_genre);
-        textview_most_liked_author = view.findViewById(R.id.textview_summary_most_liked_author);
-        textview_most_disliked_author = view.findViewById(R.id.textview_summary_most_disliked_author);
-        textview_reviews = view.findViewById(R.id.textview_summary_reviews);
-        button_get_definition_of_book = view.findViewById(R.id.button_get_definition_of_book);
+        textview_most_liked_genre = view.findViewById(R.id.textview_favourite_genre);
+        textview_most_disliked_genre = view.findViewById(R.id.textview_least_favourite_genre);
+        textview_most_liked_author = view.findViewById(R.id.textview_favourite_author);
+        textview_most_disliked_author = view.findViewById(R.id.textview_least_favourite_author);
+        textview_reviews = view.findViewById(R.id.textview_summary_positive_reviews);
+        textview_books_liked_count = view.findViewById(R.id.textview_books_liked_count);
+        textview_books_disliked_count = view.findViewById(R.id.textview_books_disliked_count);
 
         authorController = new AuthorController(view.getContext());
         genreController = new GenreController(view.getContext());
         reviewsController = new ReviewsController(view.getContext());
+        bookController = new BookController(view.getContext());
+        userController = new UserController();
 
         textview_toolbar_title.setText("Summary");
 
@@ -85,30 +90,37 @@ public class SummaryFragment extends Fragment {
         textview_most_liked_author.setText(authorController.getTopAuthorFromPairData(mostLikedAuthorData));
         textview_most_disliked_author.setText(authorController.getTopAuthorFromPairData(mostDislikedAuthorData));
 
-        button_get_definition_of_book.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                StringBuilder sb = new StringBuilder();
-                reviewsController.execute();
+        userId = userController.getUserIdFromSharedPreferneces(view.getContext());
+        books_liked_count = bookController.getBooksUsingStatus(userId, BookController.BookStatus.Liked, null).size();
+        books_disliked_count = bookController.getBooksUsingStatus(userId, BookController.BookStatus.Disliked, null).size();
 
-                while(reviewsController.getPositiveReviewsList() == null){
-                    //Do nothing until it Returns a list of reviews or an empty list
+        if(books_liked_count>0) {
+            textview_books_liked_count.setText(books_liked_count + " books");
+        }
 
-                }
-                reviews = reviewsController.getPositiveReviewsList();
-                if(reviews.size()>0) {
-                    for (String review : reviews) {
-                        //Have are 2 lines between each review to ensure adequate spacing
-                        sb.append(review + "\n\n");
-                    }
+        if(books_disliked_count>0) {
+            textview_books_disliked_count.setText(books_disliked_count + " books");
+        }
 
-                    textview_reviews.setText(sb.toString());
-                } else{
-                    textview_reviews.setText("Unavailable");
-                }
-                button_get_definition_of_book.setVisibility(View.INVISIBLE);
+        StringBuilder sb = new StringBuilder();
+        reviewsController.execute();
+
+        while(reviewsController.getPositiveReviewsList() == null){
+            //Do nothing until it Returns a list of reviews or an empty list
+
+        }
+        reviews = reviewsController.getPositiveReviewsList();
+        if(reviews.size()>0) {
+            for (String review : reviews) {
+                //Have are 2 lines between each review to ensure adequate spacing
+                sb.append(review + "\n\n");
             }
-        });
+
+            textview_reviews.setText(sb.toString());
+        } else{
+            textview_reviews.setText("Unavailable");
+        }
+
 
     }
 }
