7262f11b7e74866fbf974e3151edd51340143a89
Kane <95705546+kaneran@users.noreply.github.com>
2022-03-19 20:53:18 +0000

    Update Utils.java

--- app/src/main/java/extras/Utils.java
+++ app/src/main/java/extras/Utils.java
@@ -1,6 +1,6 @@
 package extras;
 
 public class Utils {
-    public static final String EMAIL = "koobook.donotreply@gmail.com";
-    public static final String PASSWORD = "a&%4Qd74W1&c";
+    public static final String EMAIL = "";
+    public static final String PASSWORD = "";
 }
